7dab47c2bb745890034b741af97f5f5d38bb091d
jaison080 <jaisondennis080@gmail.com>
2022-05-29 01:02:14 +0530

    Inital commit

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import ButtonGroup from '../elements/ButtonGroup';
 import Button from '../elements/Button';
-import Image from '../elements/Image';
-import Modal from '../elements/Modal';
+
 
 const propTypes = {
   ...SectionProps.types
@@ -25,17 +24,6 @@ const Hero = ({
   ...props
 }) => {
 
-  const [videoModalActive, setVideomodalactive] = useState(false);
-
-  const openModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(true);
-  }
-
-  const closeModal = (e) => {
-    e.preventDefault();
-    setVideomodalactive(false);
-  }   
 
   const outerClasses = classNames(
     'hero section center-content',
@@ -72,7 +60,7 @@ const Hero = ({
                   <Button tag="a" color="primary" wideMobile href="https://fineazy.netlify.app">
                     Login with Binance
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="/">
+                  <Button tag="a" color="dark" wideMobile href="https://fineazy.netlify.app">
                     Connect Wallet
                     </Button>
                 </ButtonGroup>
